a91958c0600d828bb213cc720f6cc2642b3eb314
aquilax <aquilax@gmail.com>
2014-06-05 20:37:13 +0200

    Fixed more cases

--- tripcode.go
+++ tripcode.go
@@ -18,18 +18,21 @@ package tripcode
 import (
 	"github.com/nyarlabo/go-crypt"
 	"github.com/qiniu/iconv"
-	"regexp"
 	"strings"
 )
 
-// Tripcode function generates tripcode for the provided password
-func Tripcode(password string) string {
+const SALT_TABLE = ".............................................../0123456789ABCDEFGABCDEFGHIJKLMNOPQRSTUVWXYZabcdefabcdefghijklmnopqrstuvwxyz....................................................................................................................................."
+
+func sjisToUtf8(text string) string {
 	cd, err := iconv.Open("SJIS", "utf-8")
+	defer cd.Close()
 	if err != nil {
 		panic("iconv.Open failed!")
 	}
-	defer cd.Close()
-	password = cd.ConvString(password)
+	return cd.ConvString(text)
+}
+
+func htmlEscape(text string) string {
 	r := strings.NewReplacer(
 		"&", "&amp;",
 		"\"", "&quot;",
@@ -37,31 +40,38 @@ func Tripcode(password string) string {
 		"<", "&lt;",
 		">", "&gt;",
 	)
+	return r.Replace(text)
+}
+
+func generateSalt(password string) string {
+	var salt [2]rune
+	password = substr(password+"H.", 1, 2)
+	for i, r := range password {
+		salt[i] = rune(SALT_TABLE[r%256])
+	}
+	return string(salt[:])
+}
+
+func substr(s string, pos, length int) string {
+	runes := []rune(s)
+	l := pos + length
+	if l > len(runes) {
+		l = len(runes)
+	}
+	return string(runes[pos:l])
+}
+
+// Tripcode function generates tripcode for the provided password
+func Tripcode(password string) string {
+	password = sjisToUtf8(password)
+	password = htmlEscape(password)
 	if password == "" {
 		return password
 	}
-	password = r.Replace(password)
 	if len(password) > 8 {
-		password = password[0:8]
+		password = substr(password, 0, 8)
 	}
-	salt := (password + "H.")[1:3]
-	re := regexp.MustCompile("/[^.\\/0-9:;<=>?@A-Z\\[\\\\]\\^_`a-z]/")
-	salt = re.ReplaceAllString(salt, ".")
-	r = strings.NewReplacer(
-		":", "A",
-		";", "B",
-		"<", "C",
-		"=", "F",
-		">", "E",
-		"?", "F",
-		"@", "G",
-		"[", "a",
-		"\\", "b",
-		"]", "c",
-		"^", "d",
-		"_", "e",
-		"`", "f")
-	salt = r.Replace(salt)
+	salt := generateSalt(password)
 	code := crypt.Crypt(password, salt)
 	l := len(code)
 	return code[l-10 : l]
--- tripcode_test.go
+++ tripcode_test.go
@@ -4,6 +4,25 @@ import (
 	"testing"
 )
 
+func TestGenerateSalt(t *testing.T) {
+	cases := map[string]string{
+		"asd":        "sd",
+		"adasd":      "da",
+		"!@#$%^&*()": "G.",
+		"f}E":        ".E",
+		"©":          "H.",
+		"訛":          "H.",
+		"'":          "H.",
+	}
+	for pass, expected := range cases {
+		salt := generateSalt(pass)
+		if expected != salt {
+			t.Error("Expected "+expected+", got", salt)
+		}
+	}
+
+}
+
 func TestTripcode(t *testing.T) {
 	cases := map[string]string{
 		"asd":        "TAPy3blMsc",
